3a4e0d662a4611467e91130dc0316a7ba15fb0f7
LuchoGalluzzo <luchogalluzzo421@gmail.com>
2017-07-05 00:58:35 -0300

    Actualiza solo al personaje que ganó

--- src/main/java/mensajeria/ActualizarPersonaje.java
+++ src/main/java/mensajeria/ActualizarPersonaje.java
@@ -20,10 +20,13 @@ public class ActualizarPersonaje extends ComandoServidor{
 	public void resolver() throws IOException {
 		Gson gson = new Gson();
 		escuchaCliente.setPaquetePersonaje((PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class));
+		if(escuchaCliente.getPaquetePersonaje().isGano() == true){
 		MyRandom r = new MyRandom();
 		Item item = new Item();
 		item = Servidor.getItemsExistentes()[r.obtenerAleatorioMenorQue(10)];
 		escuchaCliente.getPaquetePersonaje().getMochila().add(item);
+		}
+
 		Servidor.getConector().actualizarPersonaje(escuchaCliente.getPaquetePersonaje());
 		
 		Servidor.getPersonajesConectados().remove(escuchaCliente.getPaquetePersonaje().getId());
